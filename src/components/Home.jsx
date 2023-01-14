0a371b9af0a9aa61ce8f72ee1f25405afb4da83b
Jahid Eljah <jahideljah@gmail.com>
2023-01-14 19:51:06 +0100

    some design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,35 @@
 import React from "react";
 
 import Navbar from "./Navbar";
+import { Box, Typography } from "@mui/material";
 
 function Home() {
   return (
-    <div>
-      <header></header>
-      <Navbar />
-      <h1>Home</h1>
-    </div>
+    <>
+      <Box
+        sx={{
+          backgroundColor: "#9e1717;",
+          /*  backgroundColor: "primary.dark",
+          "&:hover": {
+            backgroundColor: "primary.main",
+            opacity: [0.9, 0.8, 0.7],
+          }, */
+        }}
+      >
+        <Navbar />
+      </Box>
+      <Typography
+        alignItems="center"
+        fontSize="24px"
+        fontWeight="bold"
+        color="#fff"
+        pt="80px"
+      >
+        Fitness Club <br /> <br />
+        Sweat, Smile and Repeat <br />
+        Cheack out the most effective exercises
+      </Typography>
+    </>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,9 +13,24 @@ import { Stack } from "@mui/system";
 export default function Navbar() {
   return (
     <>
-      <Stack>
-        <Link to="/">Home</Link>
-        <Link to="/exercises">Exercises</Link>
+      <Stack
+        style={{ textDecoration: "none", color: "#fff", height: 100 }}
+        direction="row"
+        fontWeight="bold"
+        fontSize="20px"
+        fontStyle="italic"
+        alignItems="center"
+        spacing={8}
+      >
+        <Link to="/" style={{ textDecoration: "none", color: "#fff" }}>
+          Logo
+        </Link>
+        <Link to="/" style={{ textDecoration: "none", color: "#fff" }}>
+          Home
+        </Link>
+        <Link to="/exercises" style={{ textDecoration: "none", color: "#fff" }}>
+          Exercises
+        </Link>
       </Stack>
     </>
   );
