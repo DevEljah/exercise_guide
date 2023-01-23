901ba66d336efde506a47d4a47991185a5a40a52
Jahid Eljah <jahideljah@gmail.com>
2023-01-23 16:44:02 +0100

    ExerciseVideos and SimilarExercises components add

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+
+import Detail from "../components/Detail";
 
 const ExerciseDetail = () => {
-  return <div>ExerciseDetail</div>;
+  return (
+    <Box>
+      <Detail />
+    </Box>
+  );
 };
 
 export default ExerciseDetail;
